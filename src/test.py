716186a86a659c0cf746025753ff7745350df190
Nathan Ahrens <nathanmahrens@gmail.com>
2017-03-18 22:04:01 -0400

    Testing strip()

--- src/test.py
+++ src/test.py
@@ -19,3 +19,5 @@ print('pub loaded: ', rsa.key.PrivateKey.load_pkcs1(priv_pem, 'PEM'))
 a_key = '-----BEGIN RSA PUBLIC KEY-----\nMIGJAoGBAL3PtrjCa1Zdnn8LT75TbXe8uWLzDjxx6bSUgIgfYvCXFsVCOe9KtOUA\nP3ZXBEQtxcSuX4n0vSStIKtyhcou5/MaY9h6wbJ0PBykyYEZZwG2vkclCF2pfEWh\neiY4AIMyT+R+ubUX3LCCphJ/1dDgVCq2da6k0A6K2/f6iFysHk3LAgMBAAE=\n-----END RSA PUBLIC KEY-----\n'
 ano = rsa.key.PublicKey.load_pkcs1(a_key, 'PEM')
 print(ano)
+
+print("Hello world!\r\n".strip('\r\n'))
