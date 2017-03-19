69291b300b65a4686c3592b49cc7858f1c1a4b95
Nathan Ahrens <nathanmahrens@gmail.com>
2017-03-18 22:01:12 -0400

    Testing knew horizons

--- src/test.py
+++ src/test.py
@@ -15,3 +15,7 @@ print('priv: ', priv_pem)
 print()
 print('pub loaded: ', rsa.key.PublicKey.load_pkcs1(pub_pem, 'PEM'))
 print('pub loaded: ', rsa.key.PrivateKey.load_pkcs1(priv_pem, 'PEM'))
+
+a_key = '-----BEGIN RSA PUBLIC KEY-----\nMIGJAoGBAL3PtrjCa1Zdnn8LT75TbXe8uWLzDjxx6bSUgIgfYvCXFsVCOe9KtOUA\nP3ZXBEQtxcSuX4n0vSStIKtyhcou5/MaY9h6wbJ0PBykyYEZZwG2vkclCF2pfEWh\neiY4AIMyT+R+ubUX3LCCphJ/1dDgVCq2da6k0A6K2/f6iFysHk3LAgMBAAE=\n-----END RSA PUBLIC KEY-----\n'
+ano = rsa.key.PublicKey.load_pkcs1(a_key, 'PEM')
+print(ano)
